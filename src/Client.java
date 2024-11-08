93dced9fa9e88a6ce9aa7b74677d0679fb8e675e
Hikkiphisan <162010903+Hikkiphisan@users.noreply.github.com>
2024-11-08 11:56:51 +0700

    sdvdshsgdfdgnfgfvbn

--- src/Client.java
+++ src/Client.java
@@ -1,12 +1,30 @@
 public class Client {
     public static void main(String[] args) {
-        Drink drink = new Drink("Coffee");
+        Drink coffee = new Drink("Coffee");
+        Drink tea = new Drink("[Trà Sữa trân châu], món đã được thay đổi do khách yêu cầu");
 
-        Command order = new OderADrink(drink);
-        Command cancelOrder = new CancelOrderADrink(drink);
-        HighLandApp highLandApp = new HighLandApp(order, cancelOrder);
+        // Tạo các Lệnh gọi và huỳ đặt món Coffee
+        Command orderCoffee = new OderADrink(coffee);
+        Command cancelOrderCoffee = new CancelOrderADrink(coffee);
+
+        // Tạo các Lệnh gọi và huỷ đặt món Trà Sữa trân châu
+        Command orderTea = new OderADrink(tea);
+        Command cancelOrderTea = new CancelOrderADrink(tea);
+
+        // Khởi tạo ứng dụng với lệnh gọi Coffee
+        HighLandApp highLandApp = new HighLandApp(orderCoffee, cancelOrderCoffee);
+
+        // Thực hiện lệnh gọi va huy Coffee
         highLandApp.clickOderADrink();
         highLandApp.clickCancelOrderADrink();
+
+        //ưu điểm của command design pattern: Thay đổi từ mệnh lệnh gọi và huỷ Cà Phê sang mệnh lệnh gọi và huỷ Trà sữa mà không sửa đổi phương thức clickOderADrink và clickCancelOrderADrink
+        highLandApp.setCommandOrderADrink(orderTea);
+        highLandApp.setCommandCancelOrderADrink(cancelOrderTea);
+
+        // Thực hiện lệnh gọi Trà Sữa
+        highLandApp.clickOderADrink();
+
     }
 
 }
--- src/HighLandApp.java
+++ src/HighLandApp.java
@@ -13,5 +13,26 @@ public class HighLandApp {
          System.out.println("Nguời dùng click vào nút huỷ gọi đồ uống.");
          commandCancelOrderADrink.execute();
      }
-}
+
+
 //•	Invoker : tiếp nhận những “mệnh lệnh của khách hàng” (Client) và gọi execute() của ConcreteCommand để thực thi mệnh lệnh.
+
+
+
+
+
+
+
+
+
+
+
+    // Các phương thức settẻ để thay đổi Command cho hành động gọi và hủy món khác nhau
+    public void setCommandOrderADrink(Command commandOrderADrink) {
+        this.commandOrderADrink = commandOrderADrink;
+    }
+
+    public void setCommandCancelOrderADrink(Command commandCancelOrderADrink) {
+        this.commandCancelOrderADrink = commandCancelOrderADrink;
+    }
+}
